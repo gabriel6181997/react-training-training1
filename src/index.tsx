c3f2896ee16d877ef60596354218068699f4ae61
gabriel6181997 <gabriel6181997@gmail.com>
2022-10-14 10:41:39 +0900

    Change Game from class to function components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  BoardProps,
-  IGameProps,
-  IGameState,
-  SquareProps,
-  Squares,
-} from "./type";
+import { BoardProps, HistoryState, SquareProps, Squares } from "./type";
 
 const Square: FC<SquareProps> = (props) => {
   let className = "square";
@@ -23,7 +17,6 @@ const Square: FC<SquareProps> = (props) => {
 };
 
 const Board: FC<BoardProps> = (props) => {
-
   const renderSquare = (i: number, isHighlighted: boolean) => {
     return (
       <Square
@@ -61,113 +54,99 @@ const Board: FC<BoardProps> = (props) => {
   );
 };
 
-class Game extends React.Component<IGameProps, IGameState> {
-  constructor(props: IGameProps) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-          col: 0,
-          row: 0,
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-      isAsc: true,
-    };
-  }
+const Game = () => {
+  const [history, setHistory] = useState<HistoryState>([
+    {
+      squares: Array(9).fill(null),
+      col: 0,
+      row: 0,
+    },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [isAsc, setIsAsc] = useState(true);
 
-  toggleAsc() {
-    this.setState({
-      isAsc: !this.state.isAsc,
-    });
-  }
+  const toggleAsc = () => {
+    setIsAsc(!isAsc);
+  };
 
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
+  const handleClick = (i: number) => {
+    const copiedHistory = history.slice(0, stepNumber + 1);
+    const current = copiedHistory[copiedHistory.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares)?.winner || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(
+      copiedHistory.concat([
         {
           squares: squares,
-          col: (i % 3) + 1,
-          row: Math.floor(i / 3) + 1,
+          col: i % 3,
+          row: Math.floor(i / 3),
         },
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step: number) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const settlement = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      let desc = move
-        ? "Go to move #" + move + "(" + step.col + "," + step.row + ")"
-        : "Go to game start";
-      return (
-        <li key={move}>
-          <button
-            onClick={() => this.jumpTo(move)}
-            className={this.state.stepNumber === move ? "bold" : ""}
-          >
-            {desc}
-          </button>
-        </li>
-      );
-    });
-
-    let status;
-    if (settlement) {
-      if (settlement.isDraw) {
-        status = "Draw";
-      } else {
-        status = "Winner: " + settlement.winner;
-      }
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i: number) => this.handleClick(i)}
-            winLine={settlement ? settlement.line : []}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <div>
-            <button onClick={() => this.toggleAsc()}>
-              {this.state.isAsc
-                ? "Sort in descending order"
-                : "Sort in ascending order"}
-            </button>
-          </div>
-          <ol>{this.state.isAsc ? moves : moves.reverse()}</ol>
-        </div>
-      </div>
+      ])
     );
+    setStepNumber(copiedHistory.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const current = history[stepNumber];
+  const settlement = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    let desc = move
+      ? "Go to move #" + move + "(" + step.col + "," + step.row + ")"
+      : "Go to game start";
+    return (
+      <li key={move}>
+        <button
+          onClick={() => jumpTo(move)}
+          className={stepNumber === move ? "bold" : ""}
+        >
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  let status;
+  if (settlement) {
+    if (settlement.isDraw) {
+      status = "Draw";
+    } else {
+      status = "Winner: " + settlement.winner;
+    }
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
-}
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => handleClick(i)}
+          winLine={settlement ? settlement.line : []}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <div>
+          <button onClick={() => toggleAsc()}>
+            {isAsc ? "Sort in descending order" : "Sort in ascending order"}
+          </button>
+        </div>
+        <ol>{isAsc ? moves : moves.reverse()}</ol>
+      </div>
+    </div>
+  );
+};
 
 // ========================================
 const rootElement = document.getElementById("root");
